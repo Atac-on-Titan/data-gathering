45ffc5091aa1badef02a047e1d82713a08878cb6
jonasbarth <jonas.barth.95@gmail.com>
2023-08-21 10:49:56 +0200

    #8 weather data * batch the downloading of historical weather data into weeks

--- download_historical_weather.py
+++ download_historical_weather.py
@@ -4,6 +4,7 @@ import logging
 from datetime import datetime
 from pathlib import Path
 
+import pandas as pd
 from dotenv import load_dotenv
 
 from download import download_historical_weather
@@ -31,11 +32,37 @@ if __name__ == "__main__":
 
     Path(args.output).mkdir(parents=True, exist_ok=True)
 
-    logger.info(f"Fetching historical weather data for timeframe: {args.start} - {args.end}")
-    weather_resp = download_historical_weather(args.start, args.end, city="ROMA", country="IT")
+    start_unix = args.start
+    end_unix = args.end
 
-    logger.info(f"Parsing downloaded weather data.")
-    weather_df = parse_weather(weather_resp)
+    from datetime import datetime
+
+    # Convert Unix timestamps to datetime objects
+    start = datetime.utcfromtimestamp(start_unix)
+    end = datetime.utcfromtimestamp(end_unix)
+
+    # Calculate the time difference
+    time_difference = end - start
+
+    week_seconds = 7 * 24 * 60 * 60
+
+    query_start = start_unix
+    query_end = start_unix + week_seconds
+
+    weather_dfs = []
+
+    if time_difference.days > 6:
+        while query_end < args.end:
+            logger.info(f"Fetching historical weather data for timeframe: {query_start} - {query_end}")
+            weather_resp = download_historical_weather(query_start, query_end, city="ROMA", country="IT")
+
+            logger.info(f"Parsing downloaded weather data.")
+            weather_dfs.append(parse_weather(weather_resp))
+
+            query_start = query_end
+            query_end += week_seconds
+
+    weather_df = pd.concat(weather_dfs, ignore_index=True)
 
     output_path = f"{args.output}/weather_df.feather"
 
